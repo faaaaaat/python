6450412fc7e5d53f7a4e7f5043349584129b670c
frank <frank@ebd.internal>
2017-05-31 14:56:51 +0800

    php and nginx

---
+++ php/env_start.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cd /D %~dp0
+
+;;;;;;;;;;;;;;;;;
+
+call env_stop.bat
+ping 127.0.0.1 -n 2 >nul
+
+;;;;;;;;;;;;;;;;;
+
+echo Starting PHP FastCGI ...
+cd php-5.5.6-nts-Win32-VC11-x64
+..\RunHiddenConsole php-cgi.exe -b 127.0.0.1:9000 -c php.ini
+
+echo Starting Nginx ...
+cd ../nginx-1.7.9
+..\RunHiddenConsole nginx.exe
+
+cd \sites\www.eyebuydirect.com\static
+
+
+
+
+
